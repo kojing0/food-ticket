63c0607c70fdb24f2b288c016c22506bdb32f520
當山由也 <touyamayoshiya@touyamayoshiyanoMacBook-Air.local>
2024-04-01 07:24:26 +0900

    zkatana APIからNFTs呼び出し一覧表示処理追加

--- packages/client/src/components/Orders.js
+++ packages/client/src/components/Orders.js
@@ -8,47 +8,66 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 
-function createData(image, name) {
-  return { image, name };
-}
+function Orders() {
+  const [items, setItems] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
-const rows = [
-  createData('https://illust8.com/wp-content/uploads/2021/06/coffee_cup_13713.png', 'コーヒ', 6.0),
-  createData('https://illust8.com/wp-content/uploads/2021/06/coffee_cup_13713.png', 'コーヒ', 9.0),
-  createData('https://illust8.com/wp-content/uploads/2021/06/coffee_cup_13713.png', 'コーヒ', 16.0),
-];
+  React.useEffect(() => {
+    fetchData();
+  }, []);
 
-export default function Orders() {
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://zkatana.explorer.startale.com/api/v2/tokens/0x81fFb522f3a1D2071A86736156E2E215D5e31ee0/instances', {
+        headers: {
+          'accept': 'application/json'
+        }
+      });
+      const jsonData = await response.json();
+      setItems(jsonData.items);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setLoading(false);
+    }
+  };
+
   return (
     <TableContainer component={Paper} sx={{ height: 280 }}>
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell >商品画像</TableCell>
+            <TableCell>商品画像</TableCell>
             <TableCell align="right">商品名</TableCell>
+            <TableCell align="right">商品詳細</TableCell>
             <TableCell align="right"></TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.image}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row" >
-                <img src={row.image} alt='main image description' width={70} />
-              </TableCell>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">
-                {/* NFT取得のコントラクトを実装 */}
-                <Button onClick={() => { }}>
-                  購入
-                </Button>
-              </TableCell>
+          {loading ? (
+            <TableRow>
+              <TableCell colSpan={3} align="center">Loading...</TableCell>
             </TableRow>
-          ))}
+          ) : (
+            items.map((item) => (
+              <TableRow key={item.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell component="th" scope="row">
+                  <img src={item.image_url} alt='main image description' width={70} />
+                </TableCell>
+                <TableCell align="right">{item.metadata.name}</TableCell>
+                <TableCell align="right">{item.metadata.description}</TableCell>
+                <TableCell align="right">
+                  <Button onClick={() => {}}>
+                    購入
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))
+          )}
         </TableBody>
       </Table>
     </TableContainer>
   );
 }
+
+export default Orders;
