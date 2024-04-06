0e00b55bdd89dfdd0fa48e520817c83b46cb5f55
K.M <kojing18@gmail.com>
2024-04-06 15:20:48 +0900

    modify contract address

--- packages/client/src/components/Orders.js
+++ packages/client/src/components/Orders.js
@@ -11,7 +11,7 @@ import Button from '@mui/material/Button';
 import { ethers } from "ethers";
 import abi from "../utils/MyToken.json";
 
-const contractAddress = "0x847353acF471c1cB165b38B0B39C92EB54372fB5"
+const contractAddress = "0xBD235f4136E2AC057f64A474aF036cB2549f258d"
 const contractABI = abi.abi;
 
 function Orders() {
@@ -27,7 +27,7 @@ function Orders() {
   const fetchData = async () => {
     try {
       // TODO:zkyotoのコントラクトに入れ替える
-      const response = await fetch('https://zkyoto.explorer.startale.com/api/v2/tokens/0xF1AeBea90d0Fa81151DA1f600Dc541a9D4e17146/instances', {
+      const response = await fetch('https://zkyoto.explorer.startale.com/api/v2/tokens/0xAd2750a92F9546774FC7c961B8a762E914b49f16/instances', {
         // const response = await fetch('https://zkyoto.explorer.startale.com/api/v2/tokens/0x81fFb522f3a1D2071A86736156E2E215D5e31ee0/instances', {
         headers: {
           'accept': 'application/json'
