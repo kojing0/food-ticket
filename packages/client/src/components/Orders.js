9d8ef521def6e5106e30b595f21308c127008911
K.M <kojing18@gmail.com>
2024-04-06 14:57:22 +0900

    modify contract address

--- packages/client/src/components/Orders.js
+++ packages/client/src/components/Orders.js
@@ -11,7 +11,7 @@ import Button from '@mui/material/Button';
 import { ethers } from "ethers";
 import abi from "../utils/MyToken.json";
 
-const contractAddress = "0x87C8d2175d9F0CfD4A9d4f24C990cabdc2192c15"
+const contractAddress = "0x847353acF471c1cB165b38B0B39C92EB54372fB5"
 const contractABI = abi.abi;
 
 function Orders() {
