530a5e2d7f4a014fa573cc1d32adc67a06d42b92
當山由也 <touyamayoshiya@touyamayoshiyanoMacBook-Air.local>
2024-04-06 19:03:27 +0900

    NFT読み込み処理追加

--- packages/client/pages/myNftPage.js
+++ packages/client/pages/myNftPage.js
@@ -1,23 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import './MyNftPage.css'; // スタイリング用のCSSファイル
-import Button from '@mui/material/Button';
-import HomeIcon from '@mui/icons-material/Home';
-import CouponIcon from '@mui/icons-material/LocalOffer';
-import MapIcon from '@mui/icons-material/Map';
-import CalendarIcon from '@mui/icons-material/Today';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle'; // チェックアイコンをインポート
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'; 
-import SmartphoneIcon from '@mui/icons-material/Smartphone'; // スマホのアイコンをインポート
 import Layout from '../src/components/Layout';
 import '../styles/Common.css'; // このページ用にインポート
-import MenuIcon from '../public/images/coffe_off.png';
-import TicketIcon from '../public/images/phon_off.png';
 
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 // チェックインアイコンのパス
 
 const checkInIcon = '/images/checkin.png';
 
+
 const MyNftPage = () => {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [logingAccount, setLoginAccount] = useState("");
+
+  useEffect(() => {
+    fetchData();
+    checkIfWalletIsConnected();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      // TODO:zkyotoのコントラクトに入れ替える
+      const response = await fetch(`https://zkyoto.explorer.startale.com/api/v2/tokens/0xBc7a0c56c8b45550e9CB844b8589E1BaA8dC88A2/instances`, {
+        headers: {
+          'accept': 'application/json'
+        }
+      });
+      const jsonData = await response.json();
+      setItems(jsonData.items);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setLoading(false);
+    }
+  };
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Make sure you have Metamask!")
+      } else {
+        console.log("We have the ethereum object", ethereum)
+      }
+      const accounts = await ethereum.request({ method: 'eth_accounts' })
+      if (accounts.length !== 0) {
+        console.log("Found an authorized account:", accounts[0]);
+        setLoginAccount(accounts[0])
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  const getOrderNFT = async (index) => {
+    try {
+        const walletAddress = logingAccount;
+        const tokenId = 0;
+        await contract.erc721.transfer(walletAddress, tokenId);
+        console.log("Mining...",contract.contractAddress);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   // ダミーデータの準備
   const products = [
     { id: 1, name: 'サンドイッチ', price: '500円', tickets: '残り10枚', image: '/images/sandwich.jpeg' },
@@ -37,12 +94,12 @@ const MyNftPage = () => {
 
         <div className="nft-list-container">
           <ul className="nft-list">
-            {products.map(product => (
-              <li key={product.id} className="nft-item">
-                <img src={product.image} alt={product.name} className="product-image" />
-                <h2>{product.name}</h2>
-                <p>価格: {product.price}</p>
-                <p>{product.tickets}</p>
+            {items.map((item, index) => (
+              <li key={item.id} className="nft-item" onClick={() => { getOrderNFT(index) }}>
+                <img src={item.image_url} alt={item.metadata.name} className="product-image" width="200" height="200" />
+                <h2>{item.metadata.name}</h2>
+                <p>価格: {item.price}</p>
+                <p>{item.metadata.description}</p>
                 {/* 他のNFT情報があればここに */}
               </li>
             ))}
